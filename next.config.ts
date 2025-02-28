7209d3d99bf9678a5ff5b57f9ee4e16185c8781e
wagmitt <wagmitt@gmail.com>
2025-02-28 19:04:43 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://discord.gg/usvqf8WE',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
